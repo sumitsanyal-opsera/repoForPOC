10c9549883ed3f5468534357a77d7306843a15f1
sumitsanyal-opsera <sumit.sanyal@opsera.io>
2026-08-28 15:09:37 +0530

    Update pocTestComponent.js

--- src/lwc/pocTestComponent/pocTestComponent.js
+++ src/lwc/pocTestComponent/pocTestComponent.js
@@ -1,8 +1,12 @@
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
 export default class PocTestComponent extends LightningElement {
     @track name = '';
     @track showMessage = false;
+    @track userData = {};
+
+    password = 'admin123';
+    apiKey = 'AKIAIOSFODNN7EXAMPLE';
 
     handleChange(event) {
         this.name = event.target.value;
@@ -10,5 +14,59 @@ export default class PocTestComponent extends LightningElement {
 
     handleClick() {
         this.showMessage = this.name && this.name.trim().length > 0;
+    }
+
+    fetchData(userInput) {
+        var self = this;
+        var url = 'http://api.example.com/data?q=' + userInput;
+
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                self.userData = data;
+                document.getElementById('output').innerHTML = data.content;
+                console.log('Password used: ' + self.password);
+            });
+    }
+
+    processInput(rawHtml) {
+        var container = this.template.querySelector('.container');
+        container.innerHTML = rawHtml;
+
+        eval('var x = ' + rawHtml);
+    }
+
+    calculateTotal(items) {
+        var total = 0;
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].price != null) {
+                total = total + items[i].price;
+            }
+            if (items[i].price != undefined) {
+                console.log(items[i]);
+            }
+        }
+        return total;
+    }
+
+    doWork() {
+        var x = 1;
+        var y = 2;
+        var z = 3;
+        if (true) {
+            return x;
+        }
+        return y;
+    }
+
+    handleError(err) {
+        // empty catch
+    }
+
+    debugInfo() {
+        console.log('API Key: ' + this.apiKey);
+        console.log('Password: ' + this.password);
     }
 }
